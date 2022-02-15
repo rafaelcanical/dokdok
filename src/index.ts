c81fa2607ca7ea43539850f7f0a60223c89ad01c
Rafael Morais <rafaelcanical@gmail.com>
2022-02-15 18:04:10 +0100

    Added ToC, added back to top button & Fixed empty body

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 #!/usr/bin/env ts-node
 import { promises as fs } from 'fs'
-import { request } from 'http'
 import { InsomniaNode } from './interfaces'
+//@ts-ignore
+import toc from 'markdown-toc'
 
 // Get file name from argument
 const file = process.argv.slice(2)[0]
@@ -41,7 +42,7 @@ getFileContent().then(async (fileContent) => {
     })
 
   workspaces.map((workspace) => {
-    let doc = `[![back](https://imgur.com/OHeXRAL.png)](https://github.com/Intutor/docs#table-of-contents)\n\n`
+    let doc = `[![back](https://imgur.com/OHeXRAL.png)](https://github.com/Intutor/docs#table-of-contents)\n\n<!--TOC-->\n\n`
 
     // Filter out groups and sort alphabetically
     const groups = fileContent.filter((item) => item._type === 'request_group' && item.parentId === workspace._id)
@@ -53,64 +54,121 @@ getFileContent().then(async (fileContent) => {
       const requests = fileContent.filter((item) => item._type === 'request' && item.parentId === group._id)
 
       requests.map((request) => {
-        const method = request?.method != null ? request.method : 'GET'
-        const requestURL = request.url?.replace('{{ _.apiurl }}', '')
-
         // Title
         doc += `## ${request.name}\n\n`
 
         // Method and endpoint
-        doc += `\`\`\`
-${methods[method]} ${method}: ${requestURL}
-\`\`\`\n\n`
+        doc += methodAndEndpoint(request)
 
         // Add description
         if (request.description) doc += `${request.description}\n\n`
 
         // Requires authentication
-        if (request?.authentication != null && Object.keys(request.authentication).length) {
-          doc += `_Request requires authentication_\n\n`
-        }
+        doc += checkIfRequiresAuth(request)
 
         // Add headers
-        if (request?.headers != null && request.headers.length) {
-          doc += `### Headers\n\n`
-          doc += `| Name | Value | Description |\n`
-          doc += `| --- | --- | --- |\n`
-          request.headers.map((header) => {
-            doc += `| \`${header.name}\` | ${header.value} | ${
-              header?.description != null ? header.description : ''
-            } |\n`
-          })
-          doc += `\n`
-        }
+        doc += headers(request)
 
         // Add url parameters
-        if (request?.parameters != null) {
-          const parameters = request.parameters.filter((param) => param.disabled === false)
-          if (parameters.length) {
-            doc += `### Parameters\n\n`
-            doc += `| Name | Value | Description |\n`
-            doc += `| --- | --- | --- |\n`
-            parameters.map((parameter) => {
-              doc += `| \`${parameter.name}\` | ${parameter.value} | ${
-                parameter?.description != null ? parameter.description : ''
-              } |\n`
-            })
-            doc += `\n`
-          }
-        }
+        doc += parameters(request)
 
         // Add body
-        if (request?.body != null && request?.body?.mimeType === 'application/json') {
-          doc += `### Body\n\n`
-          doc += `\`\`\`json
-${request.body.text}
-\`\`\`\n\n`
-        }
+        doc += body(request)
+
+        // Back to top button
+        doc += `[↑ Back to top ↑](#table-of-contents)\n\n`
       })
     })
+
+    const tableOfContents = toc(doc, { maxdepth: 2 }).content
+    doc = doc.replace('<!--TOC-->', `# Table of Contents\n\n${tableOfContents}`)
 
     fs.writeFile(`docs/${workspace.name}.md`, doc)
   })
 })
+
+/**
+ * Get method and endpoint
+ */
+function methodAndEndpoint(request: InsomniaNode): string {
+  const method = request?.method != null ? request.method : 'GET'
+  const requestURL = request.url?.replace('{{ _.apiurl }}', '')
+
+  const doc = `\`\`\`
+${methods[method]} ${method}: ${requestURL}
+\`\`\`\n\n`
+
+  return doc
+}
+
+/**
+ * Check if requires auth
+ */
+function checkIfRequiresAuth(request: InsomniaNode): string {
+  if (request?.authentication != null && Object.keys(request.authentication).length) {
+    return `_Request requires authentication_\n\n`
+  }
+
+  return ''
+}
+
+/**
+ * Get headers information
+ */
+function headers(request: InsomniaNode): string {
+  let doc = ''
+
+  if (request?.headers != null && request.headers.length) {
+    doc += `### Headers\n\n`
+    doc += `| Name | Value | Description |\n`
+    doc += `| --- | --- | --- |\n`
+    request.headers.map((header) => {
+      doc += `| \`${header.name}\` | ${header.value} | ${header?.description != null ? header.description : ''} |\n`
+    })
+    doc += `\n`
+  }
+
+  return doc
+}
+
+/**
+ * Get parameters information
+ */
+function parameters(request: InsomniaNode): string {
+  let doc = ''
+
+  if (request?.parameters != null) {
+    const parameters = request.parameters.filter((param) => param.disabled === false)
+    if (parameters.length) {
+      doc += `### Parameters\n\n`
+      doc += `| Name | Value | Description |\n`
+      doc += `| --- | --- | --- |\n`
+      parameters.map((parameter) => {
+        doc += `| \`${parameter.name}\` | ${parameter.value} | ${
+          parameter?.description != null ? parameter.description : ''
+        } |\n`
+      })
+      doc += `\n`
+    }
+  }
+
+  return doc
+}
+
+/**
+ * Get body information
+ */
+function body(request: InsomniaNode): string {
+  let doc = ''
+
+  if (request?.parameters != null) {
+    if (request?.body != null && request?.body?.mimeType === 'application/json' && request.body.text?.trim()?.length) {
+      doc += `### Body\n\n`
+      doc += `\`\`\`json
+${request.body.text.replaceAll(`\t`, '    ')}
+\`\`\`\n\n`
+    }
+  }
+
+  return doc
+}
